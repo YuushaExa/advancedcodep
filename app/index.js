f5f2d6dd60287ae49f039949008e27e96c811d21
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-05-30 15:56:47 +0300

    Update and rename app.js to app/index.js

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+const HOST = '0.0.0.0';
+let previewCounter = 0;
+
+app.use(bodyParser.json());
+
+app.post('/preview', (req, res) => {
+    const content = req.body.content;
+    previewCounter++;
+    const previewFilename = `preview-${previewCounter}.html`;
+    const previewFilePath = path.join(__dirname, previewFilename);
+
+    fs.writeFile(previewFilePath, content, (err) => {
+        if (err) {
+            console.error('Error writing preview file:', err);
+            res.status(500).send({ error: 'Failed to generate preview' });
+        } else {
+            res.send({ url: `http://${HOST}:${PORT}/${previewFilename}` });
+        }
+    });
+});
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
